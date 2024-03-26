765b5f4244c72827a5e65e7873b50c5c1944a597
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-03-26 07:31:58 +0100

    Fix flake config

--- flake.nix
+++ flake.nix
@@ -1,10 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
