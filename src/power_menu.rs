d48c357127804a73ed2ad42b05e8abc05bd660bf
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-07-10 07:29:40 +0200

    Fix lint

--- src/power_menu.rs
+++ src/power_menu.rs
@@ -62,7 +62,7 @@ pub enum Action {
 
 impl Display for SessionManager {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", format!("{:?}", self).to_lowercase())
+        write!(f, "{}", format!("{self:?}").to_lowercase())
     }
 }
 
